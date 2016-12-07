96f4e5565ee04c2b6f7f6b5a3e282f344c375fe2
sahilsd <sahil.deshpande91@gmail.com>
2016-12-06 22:31:44 -0500

    lasso - deadlock for sparse graph

--- src/LSR_weight.m
+++ src/LSR_weight.m
@@ -1,69 +1,89 @@
     %%data=load('');%%ur file here
     
-    A = [0 3 4 12 2;0 0 5 61 0; 0 0 0 4 0; 0 0 0 0 1;0 0 0 0 0];%%need parse data here according to different datasets
+   % A = [0 31 41 12 2;0 0 5 61 0; 0 0 0 5 0; 0 0 0 0 1;0 0 0 0 0];%%need parse data here according to different datasets
+    % A = input_adj;
     G=graph(A,'upper');
-    m=2;
-    s=2;
+    G_original = G;
+    m=10;
+    s=10;
     N = numnodes(G);
     number_edge=count_edge(G);
     B=zeros(m,number_edge);%% the A in Ax=y
+    y = zeros(1,m);
     for i=1:m
         G=graph(A,'upper');%% 
         
         score=zeros(1,N);
         Pf=zeros(1,N);
+        
+        % Find Pf for all vertices and Vc is the one with max Pf
         for ii=1:N
             score(1,ii)=score_of_vertice(ii,G,A);
             Pf(1,ii)=Pf_of_vertice(ii,G,A);
-            
-            
         end
         [maxx,Vc]=max(Pf);
+        
+        % Check all neighbors of Vc and find Pt, to get Vn
         B_edge_index=zeros(1,s);
+        
+        % add all weights of edges and store in y(i)
+        y_sumi = 0;
         for j=1:s
              G=graph(A,'upper');
              M = neighbors(G,Vc);
-             leng=length(M);
              
-            
+             % DEBUG: fprintf('#%d: %d neighbors = ', i, Vc);
+             % fprintf('%d,', M);
+             % fprintf('\n');
+             leng=length(M);
             if M~=0
                 scoren=zeros(1,leng);
-                Pt=zeros(1,leng);
+                %Pt=zeros(1,leng);
                 for jj=1:leng
                     score1=min(1,leng/degree(G,M(jj,:)))/leng;
                     score2=neighbor_weight(Vc,G,A)/((A(Vc,M(jj,:))+A(M(jj,:),Vc))*min(1,(neighbor_weight(Vc,G,A))/(neighbor_weight(M(jj,:),G,A)) ));%%
                     scoren(:,jj)=score1*score2;
-                    
-                    
                 end
+                % DEBUG: fprintf('#%d: Ptansitive = ', i);
+                % fprintf('%d,', Pt);
+                % fprintf('\n');
                 hg=sum(scoren);
                 Pt=scoren./hg;
-                [maxxx,Vn] = max(Pt);
-                Vn_neigh=neighbors(G,Vn);
+                [maxxx,Vn_id] = max(Pt);
+                Vn = M(Vn_id);
+                
+                ei = findedge(G,Vc,Vn);
+                if ei == 0
+                    fprintf('FATAL: no edge in (Vc,Vc) = (%d,%d)\n', Vc, Vn);
+                end
+                    
+                B_edge_index(1,j)=ei;  
+                y_sumi = y_sumi + G.Edges.Weight(ei);
+                % fprintf('#%d: (%d,%d) edge idx: %d, sum=%d\n', i, Vc, Vn, ei, y_sumi);
+                
+                %Vn_neigh=neighbors(G,Vn);
                 A(Vc,Vn)=0;
                 A(Vn,Vc)=0;
-                ei = findedge(G,Vc,Vn);
-                B_edge_index(1,j)=ei;
-               
-                
-                
-                
-                
-                
-                    
                 
             else
-                Vn=Vc;%%not exactly sure the syntax here
-                
-                
-                
+                % For empty neigh(Vc), proceed to next node
+                Vn=Vc;
             end
-            Vc=Vn;%not exactly sure the syntax here
-        end%%add measurement matrix here, where i got stuck
-        
+            % Proceed to next node
+            Vc=Vn;
+        end
+        % fprintf('%d, ', B_edge_index); 
+        % fprintf('\n');
+        if any(B_edge_index == 0)
+            fprintf('Nonexisting edge indices found. Skip adding to B\n');
+            continue;
+        end
+            
         B(i,B_edge_index)=1;
-        
-    
-        
+        y(i) = y_sumi;
     end
-
+B % print B
+y % print y
+[x,FitInfo] = lasso(B, y','Lambda',1)
+% use this y and B to find x using Lasso with optimization for-
+% problem: min ||x||_1 + ||Bx-y||_2^2
