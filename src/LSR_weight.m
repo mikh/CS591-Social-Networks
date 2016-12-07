f78e7e704789cb06fb9742b23caa795d3cf3d97c
sahil <sahilsd@bu.edu>
2016-12-07 16:39:27 -0500

    update Pf and score evaluation formulas, output sparse edges

--- src/LSR_weight.m
+++ src/LSR_weight.m
@@ -4,30 +4,38 @@
     % A = input_adj;
     G=graph(A,'upper');
     G_original = G;
-    m=10;
-    s=10;
+    
     N = numnodes(G);
+    m = floor(N/5);
+    s = m;
+    
     number_edge=count_edge(G);
     B=zeros(m,number_edge);%% the A in Ax=y
     y = zeros(1,m);
+    h = waitbar(0,'Please wait...');
+    
     for i=1:m
+        waitbar(i / m, h, sprintf('Please wait (%d of %d)',i, m));
         G=graph(A,'upper');%% 
         
-        score=zeros(1,N);
+        % score=zeros(1,N);
         Pf=zeros(1,N);
         
         % Find Pf for all vertices and Vc is the one with max Pf
         for ii=1:N
-            score(1,ii)=score_of_vertice(ii,G,A);
-            Pf(1,ii)=Pf_of_vertice(ii,G,A);
+            % score(1,ii)=score_of_vertex(ii,G,A);
+            [scores, Pf(1,ii)]=Pf_of_vertex(ii,G,A);
         end
+        
         [maxx,Vc]=max(Pf);
+        fprintf('#%d: Vc = %d, Pf(Vc) = %f,%f\n', i, Vc, Pf(Vc), maxx);
         
         % Check all neighbors of Vc and find Pt, to get Vn
         B_edge_index=zeros(1,s);
         
         % add all weights of edges and store in y(i)
         y_sumi = 0;
+        Uvc=zeros(1,2);%new
         for j=1:s
              G=graph(A,'upper');
              M = neighbors(G,Vc);
@@ -36,7 +44,8 @@
              % fprintf('%d,', M);
              % fprintf('\n');
              leng=length(M);
-            if M~=0
+             Uvc(1,1)=Vc;%new
+             if M~=0
                 scoren=zeros(1,leng);
                 %Pt=zeros(1,leng);
                 for jj=1:leng
@@ -55,6 +64,7 @@
                 ei = findedge(G,Vc,Vn);
                 if ei == 0
                     fprintf('FATAL: no edge in (Vc,Vc) = (%d,%d)\n', Vc, Vn);
+                    % break;
                 end
                     
                 B_edge_index(1,j)=ei;  
@@ -64,10 +74,12 @@
                 %Vn_neigh=neighbors(G,Vn);
                 A(Vc,Vn)=0;
                 A(Vn,Vc)=0;
+                Uvc(1,2)=Vn;%new
                 
             else
                 % For empty neigh(Vc), proceed to next node
-                Vn=Vc;
+                %Vn=Vc;
+                Vn=Uvc(1,1);%new
             end
             % Proceed to next node
             Vc=Vn;
@@ -82,8 +94,15 @@
         B(i,B_edge_index)=1;
         y(i) = y_sumi;
     end
-B % print B
-y % print y
-[x,FitInfo] = lasso(B, y','Lambda',1)
+    close(h);
+%B % print B
+%y % print y
+[x,FitInfo] = lasso(B, y','Lambda',1);
 % use this y and B to find x using Lasso with optimization for-
 % problem: min ||x||_1 + ||Bx-y||_2^2
+sparseEdges = find(x);
+fprintf('Found %d sparse edges\n', length(sparseEdges));
+for ii=1:length(sparseEdges)
+    [sin,sout] = findedge(G_original, sparseEdges(ii));
+    fprintf('Sparse edge %d: %d <-> %d\n', ii, sin, sout);
+end
---
+++ src/Pf_of_vertex.m
@@ -0,0 +1,25 @@
+function [su, pf]=Pf_of_vertex(nodeId,G,A)
+    N = numnodes(G);
+    
+    sumNeighbor = zeros(1,N);
+    for ii=1:N
+        M = neighbors(G, ii);
+        for jj=1:length(M)
+            sumNeighbor(ii) = sumNeighbor(ii) + A(ii, M(jj));
+        end
+    end
+    
+    su=zeros(1,N);
+    
+    for ii=1:N
+        su(1,ii)=score_of_vertex(ii, G, A, sumNeighbor);
+    end
+    S=sum(su);
+    score=su(nodeId);
+    % fprintf('%d: sum=%f, score = %f,', nodeId, S, score);
+    pf=(1-(score/S))*(1/(N-1));
+    if nodeId == 101
+        fprintf('%d: sum=%f, score = %f,', nodeId, S, score);
+        fprintf('Pf=%f\n', pf);    
+    end
+end
