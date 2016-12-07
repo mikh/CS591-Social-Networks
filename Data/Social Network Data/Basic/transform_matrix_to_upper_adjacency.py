6066377d49397e78108baeca07ec44466e68e71f
sahil <sahilsd@bu.edu>
2016-12-07 17:47:01 -0500

    format adj matrix

--- Data/Social Network Data/Basic/transform_matrix_to_upper_adjacency.py
+++ Data/Social Network Data/Basic/transform_matrix_to_upper_adjacency.py
@@ -4,10 +4,12 @@ with open('matrix_users_graph.txt', 'r') as f:
 data = [[int(q) for q in x.strip().split('\t')] for x in data]
 
 with open('matrix_users_graph_upper-format.txt', 'w') as f:
-	for ii in range(len(data)):
-		for jj in range(len(data[ii])):
-			if ii >= jj:
-				f.write("0\t")
-			else:
-				f.write("{}\t".format(data[ii][jj]))
-		f.write(';\n')
+        f.write('[ \n')
+        for ii in range(len(data)):
+                for jj in range(len(data[ii])):
+                        if ii >= jj:
+                                f.write("0\t")
+                        else:
+                                f.write("{}\t".format(data[ii][jj]))
+                f.write(';\n')
+        f.write('\n];')
